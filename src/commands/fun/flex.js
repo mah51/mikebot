a154d5ed5bcb5e069fac8cefa650af446c3437c1
mah51 <mah51@kent.ac.uk>
2020-07-04 22:38:41 +0100

    Adding vote perks :)

--- src/commands/fun/flex.js
+++ src/commands/fun/flex.js
@@ -41,9 +41,10 @@ module.exports = class FlexCommand extends Command {
       if (userData.votes.count === 0) { return this.makeError(msg, 'You haven\'t voted yet! to vote go to [top.gg](https://top.gg/bot/698459684205494353) to vote for MikeBot and use the `.vote` command to get some perks! ❤'); }
       if (Date.now() - userData.votes.cooldown > 43200000) { return this.makeError(msg, 'It has been longer than 12 hours since you last voted! to regain access to this command go and vote at [top.gg](https://top.gg/bot/698459684205494353)! and do `.vote` :P.'); }
       const embed = this.client.embeds.create('flex')
-        .setAuthor(`${msg.member.displayName}is a${['chad', 'legend', 'hottie'][Math.floor(Math.random() * 3)]}`, msg.author.displayAvatarURL({ size: 256 }))
+        .setAuthor(`${msg.member.displayName}is absolutely flexing on all you losers`, msg.author.displayAvatarURL({ size: 256 }))
         .setDescription(`**${compliments[Math.floor(Math.random() * compliments.length)].replace('{{member}}', msg.member)}**`)
-        .setFooter('And yes that is the supreme red on the side.');
+        .setTitle('F L E X I N G')
+        .setFooter('And yes that is the supreme red on the side = le big flex');
 
       return msg.say(embed);
     } catch (err) {
