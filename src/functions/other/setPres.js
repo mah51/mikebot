771df1781b394749220c15da451f61ca45a8d374
mah51 <mah51@kent.ac.uk>
2020-09-02 02:42:17 +0100

    Testing queue fix

--- src/functions/other/setPres.js
+++ src/functions/other/setPres.js
@@ -21,7 +21,7 @@ async function setPres(client, dbl) {
   const cmdCount = client.registry.commands
     .filter((command) => !command.ownerOnly && !command.hidden).size;
   if (process.env.IN_PRODUCTION === 'production') {
-    client.logger.info(`Sending info to database:${cmdCount}, ${guildCount}, ${memberCount}`);
+    client.logger.info(`Sending info to database:${cmdCount}, ${guildCount}, ${memberCount}\n${guildArray}`);
     db.collection('test').doc('testid').set({
       commands: cmdCount,
       guilds: guildCount,
